bd148359fb2cbc7aedb4f6973793334c29942239
Elizabeth Chan <lchan217@gmail.com>
2019-01-08 21:07:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = 70
+=======
+var name = "Joe"
+var height = "74"
+>>>>>>> 35057aaacc5bc5510bb9ab34d3fd9a81b4007234
  
 // Don't worry about this
 module.exports = { name, height
